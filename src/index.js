76b0567d503a8b965b9a97cc8e4088e58e37235c
Anomander-R <orhan.sabanac@gmail.com>
2022-09-19 20:38:39 +0200

    Lottie animation introduced instead of Dark/Light illustartion in Hero Section

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 const themeToggleBtn = document.getElementById('theme-toggle');
 const themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
 const themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
-const vasesDark = document.getElementById('vases-dark');
-const vasesLight = document.getElementById('vases-light');
+// const vasesDark = document.getElementById('vases-dark');
+// const vasesLight = document.getElementById('vases-light');
 
 if (
     localStorage.getItem('color-theme') === 'dark' ||
@@ -11,13 +11,13 @@ if (
   ) {
     //Show light icon
     themeToggleLightIcon.classList.remove('hidden');
-    vasesLight.classList.remove('hidden');
-    vasesDark.classList.add('hidden');
+    // vasesLight.classList.remove('hidden');
+    // vasesDark.classList.add('hidden');
   } else {
     //Show dark icon
     themeToggleDarkIcon.classList.remove('hidden');
-    vasesDark.classList.remove('hidden');
-    vasesLight.classList.add('hidden');
+    // vasesDark.classList.remove('hidden');
+    // vasesLight.classList.add('hidden');
 }
 
 //Listen for toggleButton click
@@ -27,21 +27,21 @@ function toggleMode(){
     //Toggle Icon
     themeToggleDarkIcon.classList.toggle('hidden');
     themeToggleLightIcon.classList.toggle('hidden');
-    vasesDark.classList.toggle('hidden');
-    vasesLight.classList.toggle('hidden');
+    // vasesDark.classList.toggle('hidden');
+    // vasesLight.classList.toggle('hidden');
     //If is set in localstorage
     if(localStorage.getItem('color-theme')){
         // If light, make dark and save
 
             if (localStorage.getItem('color-theme') === 'light'){
                 document.documentElement.classList.add('dark')
-                vasesLight.classList.add('hidden')
-                vasesDark.classList.remove('hidden');
+                // vasesLight.classList.add('hidden')
+                // vasesDark.classList.remove('hidden');
                 localStorage.setItem('color-theme', 'dark')
             } else {
                 document.documentElement.classList.remove('dark');
-                vasesDark.classList.add('hidden')
-                vasesLight.classList.remove('hidden')
+                // vasesDark.classList.add('hidden')
+                // vasesLight.classList.remove('hidden')
                 localStorage.setItem('color-theme', 'light')
             }
 
@@ -49,11 +49,11 @@ function toggleMode(){
         // If not in localstorage
         if(document.documentElement.classList.contains('dark')){
             document.documentElement.classList.remove('dark')
-            vasesDark.classList.add('hidden')
+            // vasesDark.classList.add('hidden')
             localStorage.setItem('color-theme', 'light')
         } else{
             document.documentElement.classList.add('dark')
-            vasesLight.classList.add('hidden')
+            // vasesLight.classList.add('hidden')
             localStorage.setItem('color-theme', 'dark')
         }
     }
